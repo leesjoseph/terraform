f3b576a5efd3742b7dcbfce14e2465b8db57b7a0
S Lee <sl31197@proton.me>
2025-03-07 22:10:25 +0900

    updated vpc module

---
+++ module/vpc/variables.tf
@@ -0,0 +1,26 @@
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+variable "private_subnet_cidrs" {
+  description = "The CIDR blocks for the private subnets"
+  type        = list(string)
+}
+
+variable "public_subnet_cidrs" {
+  description = "The CIDR blocks for the public subnets"
+  type        = list(string)
+  
+}
+
+variable "cluster_name" {
+  description = "The name of the EKS cluster"
+  type        = string
+}
+
+variable "availability_zones" {
+  description = "The availability zones for the subnets"
+  type        = list(string)
+  
+}
